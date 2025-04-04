f2e129dafef5de3f88c5d331962a51a01d13f673
Commit Pusher <some@mail.ru>
2025-04-04 04:33:26 +0300

    .env added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+const apiUrl = import.meta.env.VITE_API_URL;
 </script>
 
 <template>
   <main>
-    done
+    {{ apiUrl }}
   </main>
 </template>
