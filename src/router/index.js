e44cf9b80cf8dad8c19c8917cb2d8c97db339060
phoenix381 <phoenix381@bk.ru>
2025-04-05 07:37:01 +0300

    new deps

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/auth';
+
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -12,9 +14,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      
       component: () => import('../views/AboutView.vue'),
     },
   ],
